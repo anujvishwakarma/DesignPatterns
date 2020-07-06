3fa3f57f90210fdb6730f3f4cab0f3f2499f4959
anujvishwakarma <anujvishwakarmamnnit@gmail.com>
2020-07-06 12:43:03 +0530

    ThreadSafe Singleton Object

---
+++ src/main/java/ThreadSafeSingletonPattern/ThreadSafeSingletonObject.java
@@ -0,0 +1,23 @@
+package ThreadSafeSingletonPattern;
+
+public class ThreadSafeSingletonObject {
+
+    public static ThreadSafeSingletonObject threadSafeSingletonObject;
+
+    private ThreadSafeSingletonObject() { }
+
+    public static ThreadSafeSingletonObject getInstance(){
+        if(threadSafeSingletonObject == null){
+            synchronized (ThreadSafeSingletonObject.class){
+                if(threadSafeSingletonObject == null){
+                    threadSafeSingletonObject = new ThreadSafeSingletonObject();
+                }
+            }
+        }
+        return threadSafeSingletonObject;
+    }
+
+    public void getMessage(){
+        System.out.println("In thread safe singletonObject");
+    }
+}
